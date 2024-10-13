c28cdbb7431d5f1b2d04101d27fefd20041629df
syedahaniarizvi <syedahania629@gmail.com>
2024-10-13 00:40:09 -0700

    calculator code

---
+++ calculator.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char operation;
+    double num1, num2;
+
+    cout << "Enter an operator (+, -, *, /): ";
+    cin >> operation;
+
+    cout << "Enter two numbers: ";
+    cin >> num1 >> num2;
+
+    switch (operation) {
+        case '+':
+            cout << num1 << " + " << num2 << " = " << num1 + num2 << endl;
+            break;
+        case '-':
+            cout << num1 << " - " << num2 << " = " << num1 - num2 << endl;
+            break;
+        case '*':
+            cout << num1 << " * " << num2 << " = " << num1 * num2 << endl;
+            break;
+        case '/':
+            if (num2 != 0)
+                cout << num1 << " / " << num2 << " = " << num1 / num2 << endl;
+            else
+                cout << "Error: Division by zero is not allowed." << endl;
+            break;
+        default:
+            cout << "Error: Invalid operator." << endl;
+    }
+
+    return 0;
+}
